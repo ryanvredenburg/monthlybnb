1a62a15f6af7fb42151ad2934a720143908c0bc7
ryan <ryan@digitalerosion.com>
2016-09-19 21:03:51 -0500

    Add cities details to home index and cities index

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,28 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using monthlybnb.Models;
+
 
 namespace monthlybnb.Controllers
 {
     public class HomeController : Controller
     {
+        private DataContext db = new DataContext();
+
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return View(db.Cities.ToList());
         }
     }
 }
